5ffaed8df7e9364dd443298c155cc782c6cdffca
ajpelu <ajpelu@gmail.com>
2024-03-07 11:17:36 +0100

    identify first tracheid

--- test.R
+++ test.R
@@ -7,6 +7,19 @@ raw <- read.csv("data/DH216.csv", sep = ";") |> rownames_to_column("idcell")
 n_cell_rows <- 10  # Numero de filas de traqueidas medidas
 
 # Genera subset by year
-d <- raw |> dplyr::filter(Year == 1995) |> mutate(ybottom = BY - Height)
+d <- raw |> dplyr::filter(Year == 1997) |> mutate(ybottom = BY - Height)
 
 plot_cells(d)
+
+n_cell_rows <- 10  # Numero de filas de traqueidas medidas
+
+
+
+# Identificar dentro de ese año la bottom-left cell
+# ybottom = BY - Height
+bottom_left_cell <- d[which.min(d$ybottom),]
+
+
+p1 <- plot_cells(d)
+p1 + geom_point(data = bottom_left_cell, aes(x=X, y=Y), colour = "red")
+
